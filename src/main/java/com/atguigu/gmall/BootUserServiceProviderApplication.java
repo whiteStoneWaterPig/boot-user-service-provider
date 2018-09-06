f42ff0480c0ee579c2b60e1d45a3f875527561ac
bloom <1197299161@qq.com>
2018-09-06 21:41:38 +0800

    spring-boot dubbo

--- src/main/java/com/atguigu/gmall/BootUserServiceProviderApplication.java
+++ src/main/java/com/atguigu/gmall/BootUserServiceProviderApplication.java
@@ -1,8 +1,17 @@
 package com.atguigu.gmall;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * 1.导入依赖：
+ * 		1）导入dubbo-starter
+ * 		2）导入dubbo的其他依赖
+ */
+
+@EnableDubbo	//开启基于注解的dubbo功能
 @SpringBootApplication
 public class BootUserServiceProviderApplication {
 
--- src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/atguigu/gmall/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.atguigu.gmall.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.service.UserService;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +15,12 @@ import java.util.List;
  * 2.让服务消费者去注册中心订阅服务提供者的服务地址
  * Created by bloom on 2018/9/2.
  */
+@Service    //暴露服务
+@Component
 public class UserServiceImpl implements UserService {
     public List<UserAddress> getUserAddressList(String userId) {
-        UserAddress address1 = new UserAddress(1,"北京市","1","李老师","012313");
-        UserAddress address2 = new UserAddress(2,"北京市","2","李老师","0142412");
+        UserAddress address1 = new UserAddress(1,"北京市海淀区","1","李老师","012313");
+        UserAddress address2 = new UserAddress(2,"北京市朝阳区","2","张老师","0142412");
 
         return Arrays.asList(address1,address2);
     }
